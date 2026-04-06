0214e870fbffd929c7702b0350477392d38881f1
Daniel Eli <dee.00123@gmail.com>
2026-04-06 09:33:06 +0200

    Update to use AddJavaScriptApp and add devtunnel

--- src/LanManager.AppHost/AppHost.cs
+++ src/LanManager.AppHost/AppHost.cs
@@ -1,10 +1,17 @@
+using k8s.Models;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.LanManager_Api>("lanmanager-api");
 
-var frontend = builder.AddNpmApp("frontend", "../../frontend", "dev")
+var frontend = builder.AddJavaScriptApp("frontend", "../../frontend", "dev")
     .WithReference(api)
     .WithHttpEndpoint(port: 5173, env: "VITE_PORT")
     .WithExternalHttpEndpoints();
+
+//Add tunnel
+var publicDevTunnel = builder.AddDevTunnel("devtunnel-public")
+    .WithAnonymousAccess()
+    .WithReference(api.GetEndpoint("https"));
 
 builder.Build().Run();
